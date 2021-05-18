0e4fd280bbf86369d8121c0692848b8ef14d67e7
Eric <ericsilva328@gmail.com>
2021-05-18 20:29:43 -0300

    done

--- app/src/main/java/com/example/kotlinflow/WordRoomDatabase.kt
+++ app/src/main/java/com/example/kotlinflow/WordRoomDatabase.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Database(entities = arrayOf(Word::class), version = 1, exportSchema = false)
 abstract class WordRoomDatabase: RoomDatabase() {
@@ -15,13 +18,14 @@ abstract class WordRoomDatabase: RoomDatabase() {
         @Volatile
         private var INSTANCE : WordRoomDatabase? = null
 
-        fun getDatabase(context: Context): WordRoomDatabase{
+        fun getDatabase(context: Context, scope: CoroutineScope): WordRoomDatabase{
             return INSTANCE?: synchronized(lock = this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     WordRoomDatabase::class.java,
                     "word_database"
-                ). build()
+                ).addCallback(WordDataBaseCallBack(scope)).build()
+
                 INSTANCE = instance
 
                 instance
@@ -29,4 +33,27 @@ abstract class WordRoomDatabase: RoomDatabase() {
             }
         }
     }
+
+    private class WordDataBaseCallBack(
+        private val  scope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database->
+                scope.launch {
+                    populateDatabase(database.wordDao())
+                }
+            }
+        }
+
+        suspend fun populateDatabase(wordDao: WordDao){
+            wordDao.deleteAll()
+
+            var word = Word("Hello")
+            wordDao.insert(word)
+            word = Word("World!")
+            wordDao.insert(word)
+        }
+    }
+
 }
--- app/src/main/java/com/example/kotlinflow/WordsApplication.kt
+++ app/src/main/java/com/example/kotlinflow/WordsApplication.kt
@@ -1,9 +1,14 @@
 package com.example.kotlinflow
 
 import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlin.coroutines.coroutineContext
 
-class WordsApplication: Application() {
+class WordsApplication : Application() {
 
-    val database by lazy { WordRoomDatabase.getDatabase(this) }
+    val applicationScope = CoroutineScope(SupervisorJob())
+
+    val database by lazy { WordRoomDatabase.getDatabase(context = this, scope = applicationScope) }
     val repository by lazy { WordsRepository(database.wordDao()) }
 }
